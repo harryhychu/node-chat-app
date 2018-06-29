138d857e31d1db6d0cbf6da32eadec91b7057a2b
harry chu <harryhychu@gmail.com>
2018-06-29 11:53:09 +0800

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+	// Selectors
+	var messages = $('#messages')
+	var newMessage = messages.children('li')
+	// heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function() {
 	console.log('Connected to server');
 
@@ -18,6 +34,7 @@ socket.on('newMessage', function (message) {
 		createdAt: formattedTime
 	});
 	$('#messages').append(html);
+	scrollToBottom();
 })
 
 socket.on('newLocationMessage', function(message) {
@@ -34,6 +51,7 @@ socket.on('newLocationMessage', function(message) {
 	// a.attr('href', message.url);
 	// li.append(a);
 	$('#messages').append(html);
+	scrollToBottom();
 });
 
 $('#message-form').on('submit', function(e) {

ec7541cfc40242342f4e2c8f3a0367d1854ce44e
harry chu <harryhychu@gmail.com>
2018-07-03 11:37:48 +0800

    Add users class and update userList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,12 +18,32 @@ function scrollToBottom() {
 
 socket.on('connect', function() {
 	console.log('Connected to server');
+	var params = $.deparam(window.location.search);
+	socket.emit('join', params, function (err) {
+		if(err) {
+			alert(err);
+			window.location.href = '/';
+		} else {
+			console.log('No error');
+		}
+	});
 
 });
 
 socket.on('disconnect', function() {
 	console.log('disconnect to server');
 });
+
+socket.on('updateUserList', function (users) {
+	var ol = $('<ol></ol>');
+
+	users.forEach(function (user) {
+		ol.append($('<li></li>').text(user));
+	});
+	
+	$('#users').html(ol);
+
+})
 
 socket.on('newMessage', function (message) {
 	var formattedTime = moment(message.createdAt).format('h:mm a');
